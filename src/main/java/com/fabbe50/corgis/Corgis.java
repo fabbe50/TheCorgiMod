21cbdd6d4389506325c02ea172ed5e8093a104be
William <fabbe50@users.noreply.github.com>
2020-04-01 15:39:21 +0200

    Fixed Corgi Spawning Fixes #1

--- src/main/java/com/fabbe50/corgis/Corgis.java
+++ src/main/java/com/fabbe50/corgis/Corgis.java
@@ -1,22 +1,12 @@
 package com.fabbe50.corgis;
 
 import com.fabbe50.client.renderer.CorgiRenderer;
-import com.fabbe50.corgis.entities.CorgiEntity;
-import com.fabbe50.corgis.entities.registry.CorgiRegistry;
 import com.fabbe50.corgis.entities.registry.EntityRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityClassification;
-import net.minecraft.entity.EntityType;
-import net.minecraft.util.registry.Registry;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -27,8 +17,6 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("corgis")
@@ -52,11 +40,12 @@ public class Corgis
     }
 
     private void setupCommon(final FMLCommonSetupEvent event) {
-
+        EntityRegistry.addSpawningConditions();
+        DeferredWorkQueue.runLater(EntityRegistry::addSpawns);
     }
 
     private void setupClient(final FMLClientSetupEvent event) {
-        RenderingRegistry.registerEntityRenderingHandler(CorgiRegistry.CORGI, CorgiRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.CORGI, CorgiRenderer::new);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
