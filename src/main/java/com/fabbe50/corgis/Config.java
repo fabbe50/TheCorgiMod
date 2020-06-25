705d897588456cb298bc3dacc559ad150c27922d
William <fabbe50@users.noreply.github.com>
2020-06-25 15:06:21 +0200

    Added Config File

---
+++ src/main/java/com/fabbe50/corgis/Config.java
@@ -0,0 +1,335 @@
+package com.fabbe50.corgis;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class Config {
+    private ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+    private ForgeConfigSpec spec;
+
+    public ForgeConfigSpec getSpec() {
+        return spec;
+    }
+
+    private Corgis corgis;
+    private NormalCorgi normalCorgi;
+    private Fabbe50Corgi fabbe50Corgi;
+    private BodyguardCorgi bodyguardCorgi;
+    private BusinessCorgi businessCorgi;
+    private MelonCorgi melonCorgi;
+    private PirateCorgi pirateCorgi;
+    private SpyCorgi spyCorgi;
+    private SunglassesCorgi sunglassesCorgi;
+    private AntiCorgi antiCorgi;
+    private LoveCorgi loveCorgi;
+    private RadioactiveCorgi radioactiveCorgi;
+    private HeroCorgi heroCorgi;
+    private NerdCorgi nerdCorgi;
+
+    public Config() {
+        corgis = new Corgis();
+        normalCorgi = new NormalCorgi();
+        fabbe50Corgi = new Fabbe50Corgi();
+        bodyguardCorgi = new BodyguardCorgi();
+        businessCorgi = new BusinessCorgi();
+        melonCorgi = new MelonCorgi();
+        pirateCorgi = new PirateCorgi();
+        spyCorgi = new SpyCorgi();
+        sunglassesCorgi = new SunglassesCorgi();
+        antiCorgi = new AntiCorgi();
+        loveCorgi = new LoveCorgi();
+        radioactiveCorgi = new RadioactiveCorgi();
+        heroCorgi = new HeroCorgi();
+        nerdCorgi = new NerdCorgi();
+
+        spec = builder.build();
+    }
+
+    public Corgis getCorgis() {
+        return corgis;
+    }
+    public NormalCorgi getNormalCorgi() {
+        return normalCorgi;
+    }
+    public Fabbe50Corgi getFabbe50Corgi() {
+        return fabbe50Corgi;
+    }
+    public BodyguardCorgi getBodyguardCorgi() {
+        return bodyguardCorgi;
+    }
+    public BusinessCorgi getBusinessCorgi() {
+        return businessCorgi;
+    }
+    public MelonCorgi getMelonCorgi() {
+        return melonCorgi;
+    }
+    public PirateCorgi getPirateCorgi() {
+        return pirateCorgi;
+    }
+    public SpyCorgi getSpyCorgi() {
+        return spyCorgi;
+    }
+    public SunglassesCorgi getSunglassesCorgi() {
+        return sunglassesCorgi;
+    }
+    public AntiCorgi getAntiCorgi() {
+        return antiCorgi;
+    }
+    public LoveCorgi getLoveCorgi() {
+        return loveCorgi;
+    }
+    public RadioactiveCorgi getRadioactiveCorgi() {
+        return radioactiveCorgi;
+    }
+    public HeroCorgi getHeroCorgi() {
+        return heroCorgi;
+    }
+    public NerdCorgi getNerdCorgi() {
+        return nerdCorgi;
+    }
+
+    public class Corgis {
+        public Corgis() {
+            builder.push("corgis");
+
+            builder.pop();
+        }
+    }
+
+    public class NormalCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public NormalCorgi() {
+            builder.push("normalCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class Fabbe50Corgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public Fabbe50Corgi() {
+            builder.push("fabbe50Corgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class BodyguardCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public BodyguardCorgi() {
+            builder.push("bodyguardCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class BusinessCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public BusinessCorgi() {
+            builder.push("businessCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class MelonCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+        private final ForgeConfigSpec.BooleanValue shouldBreakMelons;
+
+        public MelonCorgi() {
+            builder.push("melonCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+            shouldBreakMelons = builder.comment("Should the Melon Corgi break melons?").define("Break", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+
+        public boolean getShouldBreakMelons() {
+            return shouldBreakMelons.get();
+        }
+    }
+
+    public class PirateCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public PirateCorgi() {
+            builder.push("pirateCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class SpyCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public SpyCorgi() {
+            builder.push("spyCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class SunglassesCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public SunglassesCorgi() {
+            builder.push("sunglassesCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class AntiCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public AntiCorgi() {
+            builder.push("antiCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class LoveCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+        private final ForgeConfigSpec.IntValue effectRange;
+        private final ForgeConfigSpec.BooleanValue particles;
+
+        public LoveCorgi() {
+            builder.push("loveCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+            effectRange = builder.comment("How long should the range be? (radius)").defineInRange("Range", 5, 0, 30);
+            particles = builder.comment("Is the particle effect active?").define("Particles", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+
+        public int getEffectRange() {
+            return effectRange.get();
+        }
+
+        public boolean getParticleEffect() {
+            return particles.get();
+        }
+    }
+
+    public class RadioactiveCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+        private final ForgeConfigSpec.IntValue effectRange;
+        private final ForgeConfigSpec.BooleanValue particles;
+
+        public RadioactiveCorgi() {
+            builder.push("radioactiveCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+            effectRange = builder.comment("How long should the range be? (radius)").defineInRange("Range", 5, 0, 30);
+            particles = builder.comment("Is the particle effect active?").define("Particles", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+
+        public int getEffectRange() {
+            return effectRange.get();
+        }
+
+        public boolean getParticleEffect() {
+            return particles.get();
+        }
+    }
+
+    public class HeroCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public HeroCorgi() {
+            builder.push("heroCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+
+    public class NerdCorgi {
+        private final ForgeConfigSpec.BooleanValue traitActive;
+
+        public NerdCorgi() {
+            builder.push("nerdCorgi");
+
+            traitActive = builder.comment("Is the trait active?").define("Active", true);
+
+            builder.pop();
+        }
+
+        public boolean getTraitActive() {
+            return traitActive.get();
+        }
+    }
+}
--- src/main/java/com/fabbe50/corgis/Corgis.java
+++ src/main/java/com/fabbe50/corgis/Corgis.java
@@ -3,7 +3,9 @@ package com.fabbe50.corgis;
 import com.fabbe50.corgis.entities.registry.EntityRegistry;
 import com.fabbe50.corgis.event.TooltipRenderEvent;
 import net.minecraftforge.fml.DeferredWorkQueue;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -15,9 +17,13 @@ import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("corgis")
 public class Corgis {
+    public static final Config config = new Config();
+
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Corgis() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, config.getSpec(), "Corgis.toml");
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupCommon);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
     }
