4dbbcc3c95efefff8d20f120846092aebbec4de8
William <fabbe50@users.noreply.github.com>
2022-10-29 16:28:52 +0200

    Updated from deprecated Spawn reg

--- src/main/java/com/fabbe50/corgimod/world/entity/EntityRegistry.java
+++ src/main/java/com/fabbe50/corgimod/world/entity/EntityRegistry.java
@@ -2,11 +2,13 @@ package com.fabbe50.corgimod.world.entity;
 
 import com.fabbe50.corgimod.CorgiMod;
 import com.fabbe50.corgimod.world.entity.animal.*;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -24,19 +26,22 @@ public class EntityRegistry {
     public static final RegistryObject<EntityType<CreeperCorgi>> CORGI_CREEPER = DEFERRED_REGISTER.register("corgi_creeper", () -> registerEntity(EntityType.Builder.of(CreeperCorgi::new, MobCategory.MONSTER).sized(1f, 0.5f), "corgi_creeper"));
     public static final RegistryObject<EntityType<LoveCorgi>> CORGI_LOVE = DEFERRED_REGISTER.register("corgi_love", () -> registerEntity(EntityType.Builder.of(LoveCorgi::new, MobCategory.CREATURE).sized(1f, 0.5f), "corgi_love"));
 
-    private static EntityType registerEntity(EntityType.Builder builder, String entityName) {
+    private static EntityType registerEntity(EntityType.Builder<?> builder, String entityName) {
         return builder.build(entityName);
     }
 
     @SubscribeEvent
-    public static void initializeAttributes(EntityAttributeCreationEvent event) {
-        SpawnPlacements.register(CORGI_NORMAL.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Corgi::checkAnimalSpawnRules);
-        SpawnPlacements.register(CORGI_ANTI.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, AntiCorgi::checkAnimalSpawnRules);
-        SpawnPlacements.register(CORGI_BODYGUARD.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, BodyguardCorgi::checkAnimalSpawnRules);
-        SpawnPlacements.register(CORGI_BUSINESS.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, BusinessCorgi::checkAnimalSpawnRules);
-        SpawnPlacements.register(CORGI_CREEPER.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, CreeperCorgi::checkMonsterSpawnRules);
-        SpawnPlacements.register(CORGI_LOVE.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, LoveCorgi::checkAnimalSpawnRules);
+    public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+        event.register(CORGI_NORMAL.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Corgi::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+        event.register(CORGI_ANTI.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, AntiCorgi::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+        event.register(CORGI_BODYGUARD.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, BodyguardCorgi::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+        event.register(CORGI_BUSINESS.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, BusinessCorgi::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+        event.register(CORGI_CREEPER.get(), SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, CreeperCorgi::checkMonsterSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+        event.register(CORGI_LOVE.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, LoveCorgi::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+    }
 
+    @SubscribeEvent
+    public static void initializeAttributes(EntityAttributeCreationEvent event) {
         event.put(CORGI_NORMAL.get(), Corgi.createAttributes().build());
         event.put(CORGI_ANTI.get(), AntiCorgi.createAttributes().build());
         event.put(CORGI_BODYGUARD.get(), BodyguardCorgi.createAttributes().build());
