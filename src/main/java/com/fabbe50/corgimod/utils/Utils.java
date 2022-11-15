ab8d56ca57725308803da5b3de013199d6ebbae7
William <fabbe50@users.noreply.github.com>
2022-11-15 23:53:46 +0100

    Utils

--- src/main/java/com/fabbe50/corgimod/utils/Utils.java
+++ src/main/java/com/fabbe50/corgimod/utils/Utils.java
@@ -1,7 +1,17 @@
 package com.fabbe50.corgimod.utils;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
 public class Utils {
     public static int ticksFromSecond(int seconds) {
         return seconds * 20;
+    }
+
+    public static int getTickTimeForSmeltingItem(int amount) {
+        return amount * 200;
     }
 }
