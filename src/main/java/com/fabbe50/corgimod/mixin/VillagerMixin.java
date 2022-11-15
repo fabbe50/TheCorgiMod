942c2ddba1089f2c704f84212750365c2567d351
William <fabbe50@users.noreply.github.com>
2022-11-15 23:55:46 +0100

    Business Corgi discount fix

--- src/main/java/com/fabbe50/corgimod/mixin/VillagerMixin.java
+++ src/main/java/com/fabbe50/corgimod/mixin/VillagerMixin.java
@@ -1,39 +1,39 @@
 package com.fabbe50.corgimod.mixin;
 
 import com.fabbe50.corgimod.world.entity.animal.BusinessCorgi;
-import net.minecraft.world.entity.ai.gossip.GossipContainer;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import org.spongepowered.asm.mixin.Final;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
 @Mixin(Villager.class)
-public class VillagerMixin {
-    @Final
-    @Shadow
-    private GossipContainer gossips;
+public abstract class VillagerMixin extends AbstractVillager {
+    public VillagerMixin(EntityType<? extends AbstractVillager> p_35267_, Level p_35268_) {
+        super(p_35267_, p_35268_);
+    }
 
-    @Inject(method = "getPlayerReputation", at = @At("HEAD"), cancellable = true)
-    public void getPlayerReputationInject(Player player, CallbackInfoReturnable<Integer> cir) {
-        System.out.println("Mixin ran");
-        List<BusinessCorgi> businessCorgis = player.getLevel().getNearbyEntities(BusinessCorgi.class, TargetingConditions.forNonCombat(), player, new AABB(player.getOnPos().offset(-5, -2, -5), player.getOnPos().offset(5, 2, 5)));
-        if (!businessCorgis.isEmpty()) {
-            for (BusinessCorgi corgi : businessCorgis) {
-                if (corgi.isOwnedBy(player)) {
-                    cir.setReturnValue(20);
+    @Inject(method = "updateSpecialPrices", at = @At("HEAD"), cancellable = true)
+    public void injectUpdateSpecialPrices(Player player, CallbackInfo ci) {
+        List<BusinessCorgi> corgis = player.getLevel().getEntitiesOfClass(BusinessCorgi.class, new AABB(player.getOnPos().offset(-5, -5, -5), player.getOnPos().offset(5, 5, 5)));
+        for (BusinessCorgi corgi : corgis) {
+            if (corgi.isOwnedBy(player)) {
+                for (MerchantOffer merchantOffer : this.getOffers()) {
+                    merchantOffer.setSpecialPriceDiff(-(merchantOffer.getCostA().getCount() / 2));
                 }
+                ci.cancel();
             }
         }
-        cir.setReturnValue(gossips.getReputation(player.getUUID(), gossipType -> {
-            return true;
-        }));
     }
 }
