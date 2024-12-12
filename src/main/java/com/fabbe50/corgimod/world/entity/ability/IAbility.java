c04d03dd2b24a5c255a8b868019d40e3f96153e2
William <fabbe50@users.noreply.github.com>
2024-12-12 16:36:39 +0100

    Clearing repository for rewrite

--- src/main/java/com/fabbe50/corgimod/world/entity/ability/IAbility.java
+++
@@ -1,9 +0,0 @@
-package com.fabbe50.corgimod.world.entity.ability;
-
-public interface IAbility {
-    default public void runAbilityAtEndOfFed() {};
-
-    default public void runAbilityWhileFed() {};
-
-    default public void runAbilityWhileHungry() {};
-}
